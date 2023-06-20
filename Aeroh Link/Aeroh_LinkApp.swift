21b9b7be4cc8b55fd70382551160ad5f28811072
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-06-20 08:38:41 +0530

    Aeroh Link IOS app Setup done using SwiftUI

---
+++ Aeroh Link/Aeroh_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aeroh_LinkApp.swift
+//  Aeroh Link
+//
+//  Created by Tanishq Patidar on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Aeroh_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
