8e2854a7e0df3a1aac9138ce8b193d7e39014c9d
Shiv Deepak Muddada <shiv@aeroh.org>
2023-06-21 22:11:58 -0700

    Initial Commit

---
+++ Aeroh Link/Aeroh_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aeroh_LinkApp.swift
+//  Aeroh Link
+//
+//  Created by Shiv Deepak on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Aeroh_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
