40fa14aa1d791c4451aef6224a5033239e9e0914
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-06-26 11:57:54 +0530

    Used NavigationView and NavigationLink for navigating to Login-Signup screens

--- Aeroh Link/View/OnboardingScreenView.swift
+++ Aeroh Link/View/OnboardingScreenView.swift
@@ -9,66 +9,70 @@ import SwiftUI
 
 struct OnboardingScreenView: View {
     var body: some View {
-        ZStack {
-                    // Background color
-                    Color(red: 0.06, green: 0.05, blue: 0.08)
-                        .edgesIgnoringSafeArea(.all)
-                    
-                    VStack(spacing: 179) {
-                        VStack(spacing: 43) {
-                            Spacer()
-                            
-                            // AerohLinkIllustration Image
-                            Image("AerohLinkIllustration")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)          .frame(maxWidth: 400)
-                                .padding([.leading, .trailing], 80)
-                            
-                            // "Aeroh Link" Text
-                            Text("Aeroh Link")
-                                .font(.title)
-                                .bold()
-                                .foregroundColor(.white)
-                            
-                            // "Make your home smarter with Aeroh Link" Text
-                            Text("Make your home smarter with Aeroh Link")
-                                .font(.body)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .frame(width: 270)
-                                .foregroundColor(Color(red: 0.75, green: 0.75, blue: 0.75))
-                        }
+        NavigationView {
+            ZStack {
+                        // Background color
+                        Color(red: 0.06, green: 0.05, blue: 0.08)
+                            .edgesIgnoringSafeArea(.all)
                         
-                        HStack(spacing: 23) {
-                            // Sign up Button
-                            Button(action: {
-                                // Handle sign up action
-                            }) {
-                                Text("Sign up")
-                                    .frame(maxWidth: .infinity, minHeight: 25)
+                        VStack(spacing: 179) {
+                            VStack(spacing: 43) {
+                                Spacer()
+                                
+                                // AerohLinkIllustration Image
+                                Image("AerohLinkIllustration")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)          .frame(maxWidth: 400)
+                                    .padding([.leading, .trailing], 80)
+                                
+                                // "Aeroh Link" Text
+                                Text("Aeroh Link")
+                                    .font(.title)
+                                    .bold()
                                     .foregroundColor(.white)
-                                    .padding()
-                                    .background(Color(red: 0.06, green: 0.05, blue: 0.08))
-                                    .cornerRadius(30)
-                                    .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 1))
+                                
+                                // "Make your home smarter with Aeroh Link" Text
+                                Text("Make your home smarter with Aeroh Link")
+                                    .font(.body)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .frame(width: 270)
+                                    .foregroundColor(Color(red: 0.75, green: 0.75, blue: 0.75))
                             }
                             
-                            // Login Button
-                            Button(action: {
-                               
-                            }) {
-                                Text("Log in")
-                                    .frame(maxWidth: .infinity, minHeight: 25)
-                                    .foregroundColor(Color(red: 0.06, green: 0.05, blue: 0.08))
-                                    .padding()
-                                    .background(Color(red: 1.00, green: 0.79, blue: 0.23))
-                                    .clipShape(Capsule())
+                            HStack(spacing: 23) {
+                                // Sign up Button
+                                NavigationLink(destination: SignupScreen().navigationBarHidden(true), label: {
+                                    
+                                    Text("Sign up")
+                                        .frame(maxWidth: .infinity, minHeight: 25)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .background(Color(red: 0.06, green: 0.05, blue: 0.08))
+                                        .cornerRadius(30)
+                                        .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 1))
+                                    
+                                })
+                                    
+                                
+                                
+                                // Login Button
+                                NavigationLink(destination: LoginScreen().navigationBarHidden(true), label: {
+                                    Text("Log in")
+                                        .frame(maxWidth: .infinity, minHeight: 25)
+                                        .foregroundColor(Color(red: 0.06, green: 0.05, blue: 0.08))
+                                        .padding()
+                                        .background(Color(red: 1.00, green: 0.79, blue: 0.23))
+                                        .clipShape(Capsule())
+                                })
+                                    
+                                
                             }
+                            .padding(.horizontal, 40)
                         }
-                        .padding(.horizontal, 40)
                     }
-                }
-        .overlay(SplashScreen()) // Overlay SplashScreen
+            .overlay(SplashScreen())
+        } // Overlay SplashScreen
 
     }
 }
