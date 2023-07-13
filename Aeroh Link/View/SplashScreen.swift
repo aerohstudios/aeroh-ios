2cd8d2388f8a7998fcc2ee3a4491c0360519eb80
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-13 09:42:54 +0530

    Changed logo's size, opacity and animation to make it more prominent

--- Aeroh Link/View/SplashScreen.swift
+++ Aeroh Link/View/SplashScreen.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct SplashScreen: View {
     @State private var isActive = false
-    @State private var size = 0.8
-    @State private var opacity = 0.5
+    @State private var size = 0.7
+    @State private var opacity = 0.1
     
     var body: some View {
         // Only show the splash screen if isActive is false
@@ -36,11 +36,12 @@ struct SplashScreen: View {
                 .opacity(opacity) // Apply opacity to the VStack
                 .onAppear {
                     // Animate the scale effect and opacity
-                    withAnimation(.easeIn(duration: 1.2)) {
-                        self.size = 0.9
-                        self.opacity = 1.0
-                    }
-                }
+                                       withAnimation(.easeOut(duration: 1.2)) {
+                                           self.size = 0.9
+                                           self.opacity = 1.0
+                                       }
+                                       
+                                    }
             }
             .onAppear {
                 // Toggle the isActive state after a delay
