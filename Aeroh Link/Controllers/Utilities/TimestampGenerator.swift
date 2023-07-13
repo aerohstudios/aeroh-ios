91b61f8f3e149448a9f5c07102e1563a4da35af5
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-13 09:02:04 +0530

    Generates timestamp with current date and time

---
+++ Aeroh Link/Controllers/Utilities/TimestampGenerator.swift
@@ -0,0 +1,16 @@
+//
+//  TimestampGenerator.swift
+//  Aeroh Link
+//
+//  Created by Tanishq Patidar on 06/07/23.
+//
+
+import Foundation
+
+class TimestampGenerator{
+    static func generateTimestamp() -> Int{
+        let timestamp = Int(Date().timeIntervalSince1970)
+        return timestamp
+    }
+}
+
