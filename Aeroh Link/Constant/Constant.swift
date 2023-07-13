65f94307df04c3511bc63d858ca9d7ed8840847e
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-13 09:20:21 +0530

    Template for constants like endpoints, client_id, secret

---
+++ Aeroh Link/Constant/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  Aeroh Link
+//
+//  Created by Tanishq Patidar on 06/07/23.
+//
+
+import Foundation
+
+let base_url = ""
+let login_url = "\(base_url)/users/sign_in"
+let signup_url = "\(base_url)/users"
+let client_id = ""
+let secret = ""
