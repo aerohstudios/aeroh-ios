571ad95ea29018a72fe56dec72c760b2cd78270c
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-18 10:29:25 +0530

    Made changes to optimise and renamed few things to make code more relevant

--- Aeroh Link/Controllers/SignatureController.swift
+++ Aeroh Link/Controllers/SignatureController.swift
@@ -9,34 +9,31 @@ import Foundation
 import CommonCrypto
 
 class SignatureController {
-    func hmacSha256_login(signData: signData_login, key: String) -> String {
-        let data = "\(signData.email)|\(signData.password)|\(signData.scopes)|\(signData.timestamp)"
-        let dataToHmac = data.data(using: .utf8)!
-        let keyData = key.data(using: .utf8)!
+    func login_signature(signData: LoginSignaturePayload, key: String) -> String {
+        let payloadString = "\(signData.email)|\(signData.password)|\(signData.scopes)|\(signData.timestamp)"
+        let payloadData = payloadString.data(using: .utf8)!
+        let secretKeyData = key.data(using: .utf8)!
+        let Signature = calculateHMAC(payload: payloadData, key: secretKeyData)
+        return Signature
+    }
+    func signup_signature(signData: SignupSignaturePayload, key: String) -> String {
+        let payloadString = "\(signData.email)|\(signData.password)|\(signData.first_name)|\(signData.scopes)|\(signData.timestamp)"
+        let payloadData = payloadString.data(using: .utf8)!
+        let secretKeyData = key.data(using: .utf8)!
+        let Signature = calculateHMAC(payload: payloadData, key: secretKeyData)
+        return Signature
+    }
+    
+    func calculateHMAC(payload: Data, key: Data) -> String {
+        var signature = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
         
-        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        dataToHmac.withUnsafeBytes { dataBytes in
-            keyData.withUnsafeBytes { keyBytes in
-                CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), keyBytes.baseAddress!, keyData.count, dataBytes.baseAddress!, dataToHmac.count, &digest)
+        payload.withUnsafeBytes { dataBytes in
+            key.withUnsafeBytes { keyBytes in
+                CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), keyBytes.baseAddress!, key.count, dataBytes.baseAddress!, payload.count, &signature)
             }
         }
         
-        let hmacData = Data(bytes: digest, count: Int(CC_SHA256_DIGEST_LENGTH))
-        return hmacData.map { String(format: "%02hhx", $0) }.joined()
-    }
-    func hmacSha256_signup(signData: signData_signup, key: String) -> String {
-        let data = "\(signData.email)|\(signData.password)|\(signData.first_name)|\(signData.scopes)|\(signData.timestamp)"
-        let dataToHmac = data.data(using: .utf8)!
-        let keyData = key.data(using: .utf8)!
-        
-        var digest = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        dataToHmac.withUnsafeBytes { dataBytes in
-            keyData.withUnsafeBytes { keyBytes in
-                CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), keyBytes.baseAddress!, keyData.count, dataBytes.baseAddress!, dataToHmac.count, &digest)
-            }
-        }
-        
-        let hmacData = Data(bytes: digest, count: Int(CC_SHA256_DIGEST_LENGTH))
-        return hmacData.map { String(format: "%02hhx", $0) }.joined()
-    }
+        let hmacData = Data(bytes: signature, count: Int(CC_SHA256_DIGEST_LENGTH))
+        return hmacData.map { String(format: "%02hhx", $0) }.joined()    }
+
 }
