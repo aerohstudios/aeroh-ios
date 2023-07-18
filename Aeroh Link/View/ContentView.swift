7d8f86546cd5c41e1ade187aaa50e73ab6f0bdf9
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-18 10:40:36 +0530

    Added HomeScreen and button to delete keychain and show HomeScreen when user is logged in

--- Aeroh Link/View/ContentView.swift
+++ Aeroh Link/View/ContentView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isLoggedIn = false
     
     var body: some View {
-        VStack {
-            OnboardingScreenView()
+        Group {
+            if isLoggedIn {
+                HomeScreen()
+            } else {
+                OnboardingScreenView()
+                    .onAppear {
+                        // Check if access token is present in keychain
+                        if KeychainManager.shared.getAccessToken() != nil {
+                            print("contentView",KeychainManager.shared.getAccessToken()!)
+                            // Access token exists, set isLoggedIn to true
+                            isLoggedIn = true
+                        } else {
+                            // Access token doesn't exist, set isLoggedIn to false
+                            isLoggedIn = false
+                        }
+                    }
+            }
         }
     }
 }
