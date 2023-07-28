2ea21862dd578d591c56a9b37debfd8d6ed608bc
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-07-28 21:28:25 +0530

    Seperated two constants file, AppConstants and created a template for EnvConstants

--- Aeroh Link/Constant/AppConstants.swift
+++ Aeroh Link/Constant/AppConstants.swift
@@ -10,8 +10,6 @@ import Foundation
 let base_url = ""
 let login_url = "\(base_url)/users/sign_in"
 let signup_url = "\(base_url)/users"
-let client_id = ""
-let secret = ""
 let emailRegex = #"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#
 let passwordRegex = "^(?=.*\\d)(?=.*[a-zA-Z]).{6,}$"
 let forgotPassword_url = "\(base_url)/users/password/new"
