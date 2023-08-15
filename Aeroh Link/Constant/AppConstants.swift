696c5017f9981df19b7256117ecd1801129ca5af
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-08-15 21:05:33 +0530

    Added location of email and password regex in android repo

--- Aeroh Link/Constant/AppConstants.swift
+++ Aeroh Link/Constant/AppConstants.swift
@@ -10,6 +10,7 @@ import Foundation
 let base_url = "http://localhost:3000"
 let login_url = "\(base_url)/users/sign_in"
 let signup_url = "\(base_url)/users"
+//android location for email and password regex: app/src/utils/TextValidators.java
 let emailRegex = #"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#
 let passwordRegex = "^(?=.*\\d)(?=.*[a-zA-Z]).{6,}$"
 let forgotPassword_url = "\(base_url)/users/password/new"
