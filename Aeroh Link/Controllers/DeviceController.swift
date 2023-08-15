ec336ff4114903e30d15e3cc15a0393f0cb4f9cf
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-08-15 21:27:32 +0530

    Made changes so that device is fetched only once

--- Aeroh Link/Controllers/DeviceController.swift
+++ Aeroh Link/Controllers/DeviceController.swift
@@ -9,13 +9,19 @@ import Foundation
 
 class DevicesController: ObservableObject {
     @Published var devices: [DeviceModel] = []
+    var isFetchingDevices = false
     
-    func fetchDevices(accessToken: String) {
+    func fetchDevicesIfNeeded(accessToken: String) {
+        guard !isFetchingDevices else {
+            return
+        }
+        isFetchingDevices = true
         APIManager.shared.fetchDevices(with: accessToken, errorCallback: { errorMessage in
             print(errorMessage)
         }, completion: { devices in
             DispatchQueue.main.async {
                 self.devices.append(contentsOf: devices)
+                self.isFetchingDevices = false
             }
         })
     }
