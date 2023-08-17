a84f20e017401be60a55568fee0e50f605709e76
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-08-17 16:26:56 +0530

    Added device naming screen

---
+++ Aeroh Link/View/DeviceNamingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceNamingScreen.swift
+//  Aeroh Link
+//
+//  Created by Tanishq Patidar on 16/08/23.
+//
+
+import SwiftUI
+
+struct DeviceNamingScreen: View {
+    var body: some View {
+        Text("Device naming screen")
+    }
+}
+
+struct DeviceNamingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DeviceNamingScreen()
+    }
+}
