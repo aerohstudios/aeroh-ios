096fcad76cee01c7b8c654eff5dfc765adf96045
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-09-11 20:53:01 +0530

    Added Device remote screen

---
+++ AerohLink/Views/DeviceControlScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceControlScreen.swift
+//  AerohLink
+//
+//  Created by Tanishq Patidar on 11/09/23.
+//
+
+import SwiftUI
+
+struct DeviceControlScreen: View {
+    var body: some View {
+        Text("Device control")
+    }
+}
+
+struct DeviceControlScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DeviceControlScreen()
+    }
+}
