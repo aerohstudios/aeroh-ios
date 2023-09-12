b3cf1258557bf31b13633a0882a01715d29f60f2
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-09-12 18:48:10 +0530

    Implemented UI for device control screen

--- AerohLink/Views/DeviceControlScreen.swift
+++ AerohLink/Views/DeviceControlScreen.swift
@@ -7,9 +7,61 @@
 
 import SwiftUI
 
+struct CustomButton: View {
+    let imageName: String
+    let buttonText: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 10) {
+                Image(imageName)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                Text(buttonText)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color(red: 0.06, green: 0.05, blue: 0.08))
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color(red: 1.00, green: 0.79, blue: 0.23))
+            .cornerRadius(5)
+            .padding(.horizontal)
+        }
+    }
+
+}
 struct DeviceControlScreen: View {
     var body: some View {
-        Text("Device control")
+        NavigationView {
+            ZStack(alignment: .top) {
+                Color(red: 0.06, green: 0.05, blue: 0.08)
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack(alignment: .center, spacing: 15) {
+                    CustomButton(imageName: "powerIcon", buttonText: "Power") {
+                    }
+                    CustomButton(imageName: "speedIcon", buttonText: "Speed") {
+                    }
+                    CustomButton(imageName: "plusIcon", buttonText: "Temp up") {
+                    }
+                    CustomButton(imageName: "minusIcon", buttonText: "Temp down") {
+                    }
+                }
+                .padding(.vertical)
+            }
+            .navigationBarBackButtonHidden(true)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                }) {
+                    Image(systemName: "gearshape.fill")
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        .navigationTitle("Aeroh Link")
     }
 }
 
